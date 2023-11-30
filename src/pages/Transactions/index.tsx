1259341d23d6964a7528bc5b67c115f961fbc552
Michael Matheus <michaelmatheus2001@gmail.com>
2023-11-30 10:39:36 -0300

    Commit: Estrutura visual - Componente: SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
@@ -7,15 +8,17 @@ export function Transactions() {
         <div>
             <Header />
             <Summary />
-
+            
             <TransactionsContainer>
+                <SearchForm />
+
                 <TransactionsTable>
                     <tbody>
                         <tr>
                             <td width="50%">Desenvolvimento de site</td>
                             <td>
                                 <PriceHighlight variant="income">
-                                 R$ 12.000,00
+                                    R$ 12.000,00
                                 </PriceHighlight>
                             </td>
                             <td>Venda</td>
@@ -25,7 +28,7 @@ export function Transactions() {
                             <td width="50%">Hamburguer</td>
                             <td>
                                 <PriceHighlight variant="outcome">
-                                 - R$ 59,00
+                                    - R$ 59,00
                                 </PriceHighlight>
                             </td>
                             <td>Alimentação</td>
@@ -35,7 +38,7 @@ export function Transactions() {
                             <td width="50%">Aluguel do apartamento</td>
                             <td>
                                 <PriceHighlight variant="outcome">
-                                 - R$ 1.200,00
+                                    - R$ 1.200,00
                                 </PriceHighlight>
                             </td>
                             <td>Casa</td>
