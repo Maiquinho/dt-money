cf058f13d907f70e714cb1a7b6bd5ef42b899b54
Michael Matheus <michaelmatheus2001@gmail.com>
2023-12-07 05:49:40 -0300

    Commit: Conexão com a API - Criando contexto de transações

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,41 @@
+import { ReactNode, createContext, useEffect, useState } from "react";
+
+interface Transaction {
+    id: number;
+    description: string;
+    type: 'income' | 'outcome';
+    price: number;
+    category: string;
+    createdAt: string;
+}
+
+interface TrasactionContextType {
+    transactions: Transaction[]
+}
+
+interface TransactionsProviderProps {
+    children: ReactNode
+}
+
+export const TransactionContext = createContext({} as TrasactionContextType)
+
+export function TransactionsProvider({ children }: TransactionsProviderProps){
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+
+    async function loadTransactions() {
+        const response = await fetch('http://localhost:3000/transactions');
+        const data = await response.json()
+
+        setTransactions(data)
+    }
+
+    useEffect(() => {
+        loadTransactions();
+    }, [])
+
+    return (
+        <TransactionContext.Provider value={{ transactions }}>
+            {children}
+        </TransactionContext.Provider>
+    )
+}
