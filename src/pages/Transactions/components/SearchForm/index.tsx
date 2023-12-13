9efd1226c18a86402405ed3a45d818a2bc44982e
Michael Matheus <michaelmatheus2001@gmail.com>
2023-12-13 07:41:38 -0300

    Commit: Conexão com a API - Buscando transações

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
+
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import * as z from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
+import { TransactionContext } from "../../../../contexts/TransactionsContext";
 
 
 const searchFormSchema = z.object({
@@ -12,6 +15,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm(){
+    const { fetchTransactions } = useContext(TransactionContext)
+
     const { 
         register, 
         handleSubmit,
@@ -21,9 +26,7 @@ export function SearchForm(){
     })
 
     async function handleSearchTransactions(data: SearchFormInputs){
-        await new Promise(resolve => setTimeout(resolve, 2000))
-
-        console.log(data)
+        await fetchTransactions(data.query)
     }
 
     return (
