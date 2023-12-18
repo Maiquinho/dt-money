e77180e9e9201de67aa84935933843f0ae32a067
Michael Matheus <michaelmatheus2001@gmail.com>
2023-12-18 06:31:51 -0300

    Commit: Performance - Aplicando Context Selectors

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 
 import { MagnifyingGlass } from 'phosphor-react'
 import { SearchFormContainer } from './styles'
@@ -14,7 +14,12 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionContext)
+  const fetchTransactions = useContextSelector(
+    TransactionContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
 
   const {
     register,
